1fdd60276b4dd71e75584a12e70c36c0fcee9cbf
Nevadskiy <nevadskiy@gmail.com>
2018-06-06 01:25:08 +0300

    Feature ServiceContainer

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -4,15 +4,19 @@ namespace App;
 
 class ServiceContainer {
 
-    protected $registry = [];
+    protected static $registry = [];
 
     public static function bind(string $alias, $dependency)
     {
-        self::$registry[$alials] = $dependency;
+        self::$registry[$alias] = $dependency;
     }
 
-    public static function get()
+    public static function get($alias)
     {
-       
+        if (!isset(self::$registry[$alias])) {
+            throw new \InvalidArgumentException("Service {$alias} is not binded");
+        }
+
+        return self::$registry[$alias];
     }
 }
--- tests/ServiceContainerTest.php
+++ tests/ServiceContainerTest.php
@@ -4,13 +4,48 @@ use App\ServiceContainer;
 
 class ServiceContainerTest extends TestCase
 {
+    public $service;
+
+    public function setUp()
+    {
+        $this->service = new \StdClass();
+    }
+
+    public function getPropertyValue($name)
+    {
+       $serviceReflection = new ReflectionClass(ServiceContainer::class);
+
+       $property = $serviceReflection->getProperty($name);
+       $property->setAccessible(true);
+       return $property->getValue();
+    }
+
     /** @test */
     public function can_it_bind_service()
     {
-        $service = new \StdClass();
+        ServiceContainer::bind('example', $this->service);
 
-        ServiceContainer::bind('example', $service);
+        $registry = $this->getPropertyValue('registry');
 
-        $this->assertContains($service, ServiceContainer::$registry);
+        $this->assertContains($this->service, $registry);
+    }
+
+    /** @test */
+    public function can_it_return_binded_service()
+    {
+        ServiceContainer::bind('example', $this->service);
+
+        $service = ServiceContainer::get('example');
+
+        $this->assertEquals($service, $this->service);
+    }
+
+    /** @test */
+    public function expected_wrong_service_retrieving()
+    {
+        ServiceContainer::bind('example', $this->service);
+
+        $this->expectException(InvalidArgumentException::class);
+        $service = ServiceContainer::get('another example');
     }
 }
