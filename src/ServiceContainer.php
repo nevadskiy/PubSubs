c8dfad98f243a352075511a1b9fd7e96ed7aaa25
Nevadskiy <nevadskiy@gmail.com>
2018-06-06 00:45:37 +0300

    Add example test

--- src/ServiceContainer.php
+++ src/ServiceContainer.php
@@ -1,5 +1,14 @@
 <?php
 
+namespace App;
+
 class ServiceContainer {
-    
+    public function bind(string $alias, $dependency)
+    {
+       
+    }
+    public function get()
+    {
+       
+    }
 }
